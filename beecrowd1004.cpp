27f80e1307ad146627a0cd29e37bc12c1f2cc024
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-07 15:25:42 -0300

    Simple Product

---
+++ beecrowd1004.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1 = 0;
+    int number2 = 0;
+
+    cin >> number1;
+    cin >> number2;
+
+    int product = number1 * number2;
+
+    cout << "PROD = " << product << endl;
+
+    return 0;
+}
