98d1d4fec5e85030da16556b75940a32685ee276
JulioNCavalcanti <jnc.nunescavalcanti@outlook.com>
2023-11-12 23:39:20 -0300

    Distance

---
+++ beecrowd1016.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int distance = 0;
+    cin >> distance;
+
+    int necessary_time = distance * 2;
+    cout << necessary_time << " minutos" << endl;
+
+    return 0;
+}
